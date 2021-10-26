f3e2d8e912f23946bec7672cb0ac1d0d9b11934c
Jihun SeonWoo <swjh9523@gmail.com>
2021-10-26 18:30:29 +0900

    크로아티아 알파벳

--- 파이썬 알고리즘/백준/2941.py
+++ 파이썬 알고리즘/백준/2941.py
@@ -1,10 +1,8 @@
 word = input()
 text_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-cnt = 0
 
 for k, v in enumerate(text_list):
     if v in word:
-        cnt += 1
-        word = word.replace(v, " ") # 해당 문자열을 길이 1인 공백문자로 치환
+        word = word.replace(v, " ") # 해당 문자를 길이가 1인 공백문자로 치환
 
 print(len(word))
