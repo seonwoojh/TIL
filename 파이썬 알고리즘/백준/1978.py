c4d76b505a607c85e744804cf04beee6ec2d4708
Jihun SeonWoo <swjh9523@gmail.com>
2021-11-10 10:41:47 +0900

    소수 찾기

--- 파이썬 알고리즘/백준/1978.py
+++ 파이썬 알고리즘/백준/1978.py
@@ -1,8 +1,8 @@
 N = int(input())
 num_list = [x for x in map(int, input().split()) if x != 1]
-for i in num_list:
-	 for j in range(2, num_list[-1] + 1):
-		 if (i * j) in num_list:
-			 num_list.remove(i * j)
+for i in range(2,1001):
+    for j in range(2, 1001):
+       if (i * j) in num_list:
+          num_list.remove(i * j)
 
-print(num_list)
+print(len(num_list))
