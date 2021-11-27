dc8aa7024298572d2015df6235196b1e4c510fd2
Jihun SeonWoo <swjh9523@gmail.com>
2021-11-27 14:32:14 +0900

    소인수분해

--- 파이썬 알고리즘/백준/11653.py
+++ 파이썬 알고리즘/백준/11653.py
@@ -2,35 +2,18 @@ import math
 import sys
 
 
-def prime_list(n):
-    sieve = [True] * n
-
-    m = int(n ** 0.5)
-    for i in range(2, m + 1):
-        if sieve[i] == True:
-            for j in range(i+i, n, i):
-                sieve[j] = False
-
-
-    return [i for i in range(2, n) if sieve[i] == True]
-
+# 소수를 신경쓰지 않고 iterator를 2부터 sqrt(N) 까지 증가시키며 나누어 떨어지는 경우에 출력하고 N은 몫으로 감소시킨다.
 
 N = int(sys.stdin.readline())
-div_list = prime_list(N)
-result = []
-div_N = N
+M = math.ceil(math.sqrt(N))
+i = 2
+
 while True:
-  if N == 1:
-    break
-  elif N == 2 or N == 3:
-    print(math.trunc(N))
-    break
-  for div in div_list:
-    if div_N % div == 0:
-      div_N /= div
-      result.append(div)
-    else:
-      pass
-  if div_N == 1:
-    print(*sorted(result), sep = '\n')
-    break
+    if N == 1:
+        break
+    if N % i == 0:
+        print(i)
+        N /= i
+    elif N % i != 0:
+        i += 1
+        pass
