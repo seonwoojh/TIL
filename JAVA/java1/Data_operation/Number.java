8050379a15f2ba052468dc40e9a02fc5706dadd5
seonwoojh <swjh9523@gmail.com>
2021-12-05 15:37:26 +0900

    숫자와 연산

---
+++ JAVA/java1/Data_operation/Number.java
@@ -0,0 +1,16 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		System.out.println(Math.PI); // 3.14159265...
+		System.out.println(Math.floor(Math.PI)); // 3.0
+		System.out.println(Math.ceil(Math.PI));
+	}
+
+}
