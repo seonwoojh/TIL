81d4c97fcdf58d66586ea2acedfc34a709b55924
seonwoojh <swjh9523@gmail.com>
2021-12-05 15:39:11 +0900

    문자열의 표현

---
+++ JAVA/java1/Data_operation/StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		// Character VS String
+		System.out.println("Hello World"); // String 문자열
+		System.out.println('H'); // 자바에서 작은 따옴표는 Character를 나타냄(한 글자를 표현하는 데이터 타입)
+		System.out.println("H"); // String
+		
+		// System.out.println("Hello "
+		//		+ " World"); // 줄바꿈 안됨 문자열 결합임
+		
+		// New line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\"");
+	}
+}
