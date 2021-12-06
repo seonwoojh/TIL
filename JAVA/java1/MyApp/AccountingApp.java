c1565a5225b3b7905c4393cc024c1af990cc8f44
seonwoojh <swjh9523@gmail.com>
2021-12-06 18:11:35 +0900

    My App project Accounting App

---
+++ JAVA/java1/MyApp/AccountingApp.java
@@ -0,0 +1,29 @@
+
+public class AccountingApp {
+
+	public static void main(String[] args) {
+		
+		// variable
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double vat = valueOfSupply * vatRate;
+		double expenseRate = 0.3;
+		double expense = valueOfSupply * expenseRate;
+		double Income = valueOfSupply - expense;
+		double total = valueOfSupply + vat;
+		double dividend1 = Income * 0.5;
+		double dividend2 = Income * 0.3;
+		double dividend3 = Income * 0.2;
+		
+		
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + vat);
+		System.out.println("Total : " + total);
+		System.out.println("Expense : " + expense);		
+		System.out.println("Income : " + Income);
+		System.out.println("Dividend 1 : " + dividend1);
+		System.out.println("Dividend 2 : " + dividend2);
+		System.out.println("Dividend 3 : " + dividend3);
+	}
+
+}
