f339d11f5ae3473dab5e781bd826decdc95ec70c
Jihun SeonWoo <swjh9523@gmail.com>
2021-12-12 16:49:07 +0900

    영화감독 숌

---
+++ 파이썬 알고리즘/백준/1436.py
@@ -0,0 +1,11 @@
+N = int(input())
+series_num = 666
+cnt = 0
+
+while True:
+    if '666' in str(series_num):
+        cnt += 1
+    if cnt == N:
+        print(series_num)
+        break
+    series_num += 1
