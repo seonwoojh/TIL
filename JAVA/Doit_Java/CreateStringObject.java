a4b77f16133bb6e5a144cf256efb8ab20f63184c
Jihun SeonWoo <swjh9523@gmail.com>
2022-02-06 19:52:11 +0900

    String 객체를 생성하는 2가지 방법

---
+++ JAVA/Doit_Java/CreateStringObject.java
@@ -0,0 +1,11 @@
+public class CreateStringObject {
+    public static void main(String[] args) {
+        // String 객체 생성 1`
+        String str1 = new String("안녕");
+        System.out.println(str1); // new 키워드와 생성자를 이용함
+
+        // String 객체 생성 2
+        String str2 = "안녕하세요";
+        System.out.println(str2);
+    }
+}
