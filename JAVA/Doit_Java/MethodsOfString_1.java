072672041743a3304d08c24d95b49ca1e67f48f4
seonwoojh <swjh9523@gmail.com>
2022-02-11 23:14:11 +0900

    String 클래스의 주요 메서드1

---
+++ JAVA/Doit_Java/MethodsOfString_1.java
@@ -0,0 +1,28 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.util.Arrays;
+
+public class MethodsOfString_1 {
+    public static void main(String[] args) {
+        // 문자열 길이
+        String str1 = "Hello Java";
+        String str2 = "안녕하세요! 반갑습니다.";
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println();
+
+        // 문자열 검색
+        // @charAt()
+        System.out.println(str1.charAt(1));
+        System.out.println(str2.charAt(1));
+        System.out.println();
+
+        // @indexOf(), lastIndexOf()
+        System.out.println(str1.indexOf('a'));
+        System.out.println(str1.lastIndexOf('a'));
+        System.out.println(str1.indexOf('a', 8));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+}
